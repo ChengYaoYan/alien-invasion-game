a54f9be8a079bdb538ebdde10fa2489974605c58
ChengYaoYan <1526695730@qq.com>
2021-03-02 15:36:08 +0800

    finish end game part

--- src/settings.py
+++ src/settings.py
@@ -8,10 +8,17 @@ class Settings:
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
 		self.ship_speed = 1
+		self.ship_limit = 2
 
 		# Bullet settings
 		self.bullet_speed = 1.0
-		self.bullet_width = 3
+		self.bullet_width = 10
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
-		self.bullets_allowed = 3
+		self.bullets_allowed = 10
+
+		# Alien settings
+		self.alien_speed = 0.5
+		self.fleet_drop_speed = 40
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
